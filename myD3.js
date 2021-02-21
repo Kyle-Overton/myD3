8df777c031fe49d8256f77c0ad281f30a490558a
Kyle Overton <ko@kyleoverton.com>
2021-02-21 11:33:45 -0500

    Added HP to local

--- myD3.js
+++ myD3.js
@@ -3,3 +3,6 @@ d3.csv("/01-load/data/harry_potter.csv").then(res =>{
     console.log('Local csv:', res);
 });
 
+d3.csv("harry_potter_local.csv").then(res =>{
+    console.log('Local csv:', res);
+});

7a28558b3e3a96602eb8793b04bf913f75ef9eb6
Kyle Overton <ko@kyleoverton.com>
2021-02-21 18:06:02 -0500

    added split sort

--- myD3.js
+++ myD3.js
@@ -57,7 +57,7 @@ function type(d) {
 //Main function.
 function ready(movies) {
     const moviesClean = filterData(movies);
-    const barChartData = prepareBarChartData(moviesClean).sort((a, b) => {
+    const barChartData = prepareBarChartData(moviesClean).split(/\s/).sort((a, b) => {
         return d3.descending(a.revenue - b.revenue);
     });
     console.log(barChartData);

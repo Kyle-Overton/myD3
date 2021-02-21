1202b20d50a32d07fa2ee4906a4754c0d59cca03
Kyle Overton <ko@kyleoverton.com>
2021-02-21 13:07:11 -0500

    update to index

--- myD3.js
+++ myD3.js
@@ -1,5 +1,5 @@
 
-console.log("my NewMovie");
+console.log("my NewMovie Dos");
 const parseNA = string => (string === 'NA' ? undefined:string);
 const parseDate = string => d3.timeParse('%Y-%m-%d')(string);
 //Type Conversion

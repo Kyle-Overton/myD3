488f28c3e1c5affcaa17f3fb3f8c46f9f6b1e171
Kyle Overton <ko@kyleoverton.com>
2021-02-21 12:40:29 -0500

    Update myD3.js

--- myD3.js
+++ myD3.js
@@ -1,5 +1,31 @@
-console.log("hello world movies");
 
+console.log("hello world movies");
+const parseNA = string => (string === 'NA' ? undefined:string)
+const parseDate = string => d3.timeParse('%Y-%m-%d')(string);
+//Type Conversion
+function type(d){
+    return{
+        budget:+d.budget, 
+        genre:parseNA(d.genre), 
+        genres:d.genres, 
+        homepage:parseNA(d.homepage), 
+        id:+d.id, 
+        imdb_id:parseNA(d.imdb_id), 
+        original_language:d.original_language, 
+        overview:parseNA(d.overview), 
+        popularity:+d.popularity, 
+        poster_path:parseNA(d.poster_path), 
+        production_countries:d.production_countries, 
+        release_date: parseDate(d.release_date), 
+        revenue:+d.revenue, 
+        runtime:+d.runtime, 
+        tagline:parseNA(d.tagline), 
+        title:parseNA(d.title), 
+        vote_average:+d.vote_average, 
+        vote_count:d.vote_count, 
+    };
+}
+//Load data.
 d3.csv('/myD3/03/demos/module-03/before/02-prepare/data/movies.csv').then(res =>{
     console.log(res);
 })

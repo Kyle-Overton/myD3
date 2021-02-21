62defdef033c61a6c4191c832ed19b57689b688f
Kyle Overton <ko@kyleoverton.com>
2021-02-21 11:47:49 -0500

    Update myD3.js

--- myD3.js
+++ myD3.js
@@ -1,8 +1,5 @@
 console.log("hello world2");
-d3.csv("/myD3/01-load/data/harry_potter.csv").then(res =>{
-    console.log('Local csv:', res);
-});
-
-d3.csv("harry_potter_local.csv").then(res =>{
-    console.log('Local csv:', res);
-});
+d3.json("https://api.themoviedb.org/3/movie/550?api_key=3da51145ce2ec4a7e006cbba91c0f016"
+).then(res =>{
+    console.log('API json', res);
+})

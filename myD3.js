96607772618470cd5fbc36ddbb84bc7691044e11
Kyle Overton <ko@kyleoverton.com>
2021-02-21 17:19:18 -0500

    Pulled Type Conversion

--- myD3.js
+++ myD3.js
@@ -26,6 +26,34 @@ function prepareBarChartData(data) {
     }));
     return dataArray;
 }
+// Data utilities.
+const parseNA = string => (string === 'NA' ? undefined : string);
+const parseDate = string => d3.timeParse('%Y-%m-%d')(string);
+//Type Conversion
+function type(d) {
+    const date = parseDate(d.release_date);
+    return {
+        budget: +d.budget,
+        genre: parseNA(d.genre),
+        genres: JSON.parse(d.genres).map(d => d.name),
+        homepage: parseNA(d.homepage),
+        id: +d.id,
+        imdb_id: parseNA(d.imdb_id),
+        original_language: d.original_language,
+        overview: parseNA(d.overview),
+        popularity: +d.popularity,
+        poster_path: parseNA(d.poster_path),
+        production_countries: JSON.parse(d.production_countries),
+        release_date: date,
+        release_year: date.getFullYear(),
+        revenue: +d.revenue,
+        runtime: +d.runtime,
+        tagline: parseNA(d.tagline),
+        title: parseNA(d.title),
+        vote_average: +d.vote_average,
+        vote_count: d.vote_count
+    };
+}
 //Main function.
 function ready(movies) {
     const moviesClean = filterData(movies);
@@ -34,34 +62,7 @@ function ready(movies) {
     });
     console.log(barChartData);
 
-    // Data utilities.
-    const parseNA = string => (string === 'NA' ? undefined : string);
-    const parseDate = string => d3.timeParse('%Y-%m-%d')(string);
-    //Type Conversion
-    function type(d) {
-        const date = parseDate(d.release_date);
-        return {
-            budget: +d.budget,
-            genre: parseNA(d.genre),
-            genres: JSON.parse(d.genres).map(d => d.name),
-            homepage: parseNA(d.homepage),
-            id: +d.id,
-            imdb_id: parseNA(d.imdb_id),
-            original_language: d.original_language,
-            overview: parseNA(d.overview),
-            popularity: +d.popularity,
-            poster_path: parseNA(d.poster_path),
-            production_countries: JSON.parse(d.production_countries),
-            release_date: date,
-            release_year: date.getFullYear(),
-            revenue: +d.revenue,
-            runtime: +d.runtime,
-            tagline: parseNA(d.tagline),
-            title: parseNA(d.title),
-            vote_average: +d.vote_average,
-            vote_count: d.vote_count
-        };
-    }
+
     // Margin convention.
     const margin = {
         top: 40,

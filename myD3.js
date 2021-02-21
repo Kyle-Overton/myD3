d7e068cc6ffaa0b100b14ef91a0e9ff1cf9bb96a
Kyle Overton <ko@kyleoverton.com>
2021-02-21 12:11:06 -0500

    All Exercise Files

--- myD3.js
+++ myD3.js
@@ -1,9 +1,5 @@
-console.log("hello world potter/rings");
+console.log("hello world movies");
 
-const potter = d3.csv('/myD3/01-load/data/harry_potter.csv');
-const rings = d3.csv('/myD3/01-load/data/lord_of_the_rings.csv');
-
-Promise.all([potter,rings]).then(res =>{
-    console.log('Multiple requests: ', res);
-    console.log('Multiple requests concat: ', [...res[0], ...res[1]]);
+d3.csv('/myD3/03/demos/module-03/before/02-prepare/data/movies.csv').then(res =>{
+    console.log(res);
 })

61ff1a7735fdf2900af36f5ef5539cb7ad50475b
Kyle Overton <ko@kyleoverton.com>
2021-02-21 13:01:42 -0500

    my NewMovie2

--- myD3.js
+++ myD3.js
@@ -1,13 +1,13 @@
 
 console.log("my NewMovie");
-const parseNA = string => (string === 'NA' ? undefined:string)
+const parseNA = string => (string === 'NA' ? undefined:string);
 const parseDate = string => d3.timeParse('%Y-%m-%d')(string);
 //Type Conversion
 function type(d){
     return{
         budget:+d.budget, 
         genre:parseNA(d.genre), 
-        genres:d.genres, 
+        genres: JSON.parse(d.genres).map(d => d.name), 
         homepage:parseNA(d.homepage), 
         id:+d.id, 
         imdb_id:parseNA(d.imdb_id), 
@@ -15,7 +15,7 @@ function type(d){
         overview:parseNA(d.overview), 
         popularity:+d.popularity, 
         poster_path:parseNA(d.poster_path), 
-        production_countries:d.production_countries, 
+        production_countries: JSON.parse(d.production_countries), 
         release_date: parseDate(d.release_date), 
         revenue:+d.revenue, 
         runtime:+d.runtime, 
@@ -26,6 +26,6 @@ function type(d){
     };
 }
 //Load data.
-d3.csv('/myD3/03/demos/module-03/before/02-prepare/data/movies.csv').then(res =>{
+d3.csv('/myD3/03/demos/module-03/before/02-prepare/data/movies.csv', type).then(res =>{
     console.log(res);
 })
